39792df6cec01813718fe8398e1cf8e18c9f1013
Oskar Eriksen <oska1001@stud.kea.dk>
2024-11-04 10:41:09 +0100

    ai projekt

---
+++ src/main/java/dk/kea/miniaimvp/MiniAiMvpApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.miniaimvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAiMvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniAiMvpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/miniaimvp/MiniAiMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.miniaimvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAiMvpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
